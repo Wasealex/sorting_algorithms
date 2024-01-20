58847ffe53f1dcf2f7bed81b2283d64c67b91aae
Wasealex <wasealex27@gmail.com>
2024-01-20 12:42:28 +0300

    created bubble sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,6 +7,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	int temp;
+	size_t idx, j;
+
 	if (array == NULL || size == 0)
 		return;
+	for (idx = 0; idx < size; idx++)
+	{
+		for (j = 0; j < size - idx - 1; j++)
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		print_array(array, size);
+	}
 }
