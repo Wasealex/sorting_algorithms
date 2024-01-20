f403ae1360f2dd4a383c395e61efc417cd581e9c
Wasealex <wasealex27@gmail.com>
2024-01-20 11:47:55 +0300

    trial checker

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ *bubble_sort - sort an array using bubble sort
+ *@array: numbers to be sorted
+ *@size: size of array
+ *Return: Void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	if (array == NULL || size == 0)
+		return;
+}
