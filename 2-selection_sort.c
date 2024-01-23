ff8467bf37f71b97181a4e0cf4f6ec8cfde9241f
Wasealex <wasealex27@gmail.com>
2024-01-23 11:03:26 +0300

    print only the changed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,6 +10,9 @@ void selection_sort(int *array, size_t size)
 	size_t idx, min_idx, j;
 	int temp;
 
+	if (array == NULL || size == 0)
+		return;
+
 	for (idx = 0; idx < size - 1; idx++)
 	{
 		min_idx = idx;
@@ -18,9 +21,12 @@ void selection_sort(int *array, size_t size)
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
-		temp = array[min_idx];
-		array[min_idx] = array[idx];
-		array[idx] = temp;
-		print_array(array, size);
+		if (array[idx] > array[min_idx])
+		{
+			temp = array[min_idx];
+			array[min_idx] = array[idx];
+			array[idx] = temp;
+			print_array(array, size);
+		}
 	}
 }
