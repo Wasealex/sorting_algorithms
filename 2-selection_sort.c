4459f866f3eb57ad2ba3e00618a07700846b80fc
Wasealex <wasealex27@gmail.com>
2024-01-23 10:59:27 +0300

    selection_sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ *selection_sort - uses sorting algorithm (selection sort)
+ *@array: unsorted array
+ *@size: size of the array
+ *Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t idx, min_idx, j;
+	int temp;
+
+	for (idx = 0; idx < size - 1; idx++)
+	{
+		min_idx = idx;
+		for (j = idx + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		temp = array[min_idx];
+		array[min_idx] = array[idx];
+		array[idx] = temp;
+		print_array(array, size);
+	}
+}
